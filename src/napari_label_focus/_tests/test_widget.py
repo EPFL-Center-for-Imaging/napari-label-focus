4e19d672c9c8e446cd5e13543b3c37129ac2fe91
Mallory Wittwer <mallory.wittwer@epfl.ch>
2022-11-28 11:39:01 +0100

    Add little test

--- src/napari_label_focus/_tests/test_widget.py
+++ src/napari_label_focus/_tests/test_widget.py
@@ -1,51 +1,16 @@
 import numpy as np
 
-from napari_label_focus import TableGeneratorWidget #ExampleQWidget, example_magic_widget
+from napari_label_focus import TableGeneratorWidget
 
 
 def test_example_q_widget(make_napari_viewer, capsys):
     # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
+    test_labels = np.arange(0, 9).reshape((3, 3))
+    viewer.add_labels(test_labels)
 
     # create our widget, passing in the viewer
     my_widget = TableGeneratorWidget(viewer)
 
-    # call our widget method
-    my_widget._on_click()
+    assert len(my_widget.table._table) == len(np.unique(test_labels)) - 1
 
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"
-
-# # make_napari_viewer is a pytest fixture that returns a napari viewer object
-# # capsys is a pytest fixture that captures stdout and stderr output streams
-# def test_example_q_widget(make_napari_viewer, capsys):
-#     # make viewer and add an image layer using our fixture
-#     viewer = make_napari_viewer()
-#     viewer.add_image(np.random.random((100, 100)))
-
-#     # create our widget, passing in the viewer
-#     my_widget = ExampleQWidget(viewer)
-
-#     # call our widget method
-#     my_widget._on_click()
-
-#     # read captured output and check that it's as we expected
-#     captured = capsys.readouterr()
-#     assert captured.out == "napari has 1 layers\n"
-
-
-# def test_example_magic_widget(make_napari_viewer, capsys):
-#     viewer = make_napari_viewer()
-#     layer = viewer.add_image(np.random.random((100, 100)))
-
-#     # this time, our widget will be a MagicFactory or FunctionGui instance
-#     my_widget = example_magic_widget()
-
-#     # if we "call" this object, it'll execute our function
-#     my_widget(viewer.layers[0])
-
-#     # read captured output and check that it's as we expected
-#     captured = capsys.readouterr()
-#     assert captured.out == f"you have selected {layer}\n"
